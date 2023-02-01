9b1d4504d3ee4aa40d326315f7c76be618bcbb7f
Andres Felipe Serrano Barrios <andresfserrano1@gmail.com>
2023-02-01 15:46:49 -0500

    Iniciando proyecto birds

---
+++ src/main/java/com/ias/BirdsProject/BirdsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ias.BirdsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ias/BirdsProject/BirdsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ias.BirdsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
